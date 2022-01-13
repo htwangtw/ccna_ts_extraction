0abd960003dd0c0fb2958d98af4cdb4b8df44335
Hao-Ting Wang <htwangtw@gmail.com>
2022-01-13 10:18:11 -0500

    [ENH] separate difumo into regions
    To match PB reuqest for recent tutorial

---
+++ difumo_extract_region.py
@@ -0,0 +1,35 @@
+"""Demo for extracting regions from probability atlas with networks."""
+
+from nilearn import plotting
+
+from nilearn.dataset import fetch_atlas_difumo
+from nilearn.image import index_img
+from nilearn.plotting import find_xyz_cut_coords
+from nilearn.regions import RegionExtractor
+
+
+atlas = fetch_atlas_difumo()
+
+
+extractor = RegionExtractor(atlas.maps, threshold=0.5,
+                            thresholding_strategy='ratio_n_voxels',
+                            extractor='local_regions',
+                            standardize=True)
+extractor.fit()
+regions_extracted_img = extractor.regions_img_
+
+regions_index = extractor.index_
+n_regions_extracted = regions_extracted_img.shape[-1]
+plotting.plot_prob_atlas(regions_extracted_img,
+                         view_type='contours',
+                         title=f"{n_regions_extracted} from difumo 64")
+
+for i in range(64):
+    regions_indices_network = np.where(np.array(extractor.index_) == i)
+    for index in regions_indices_network[0]:
+        cur_img = index_img(extractor.regions_img_, index)
+        coords = find_xyz_cut_coords(cur_img)
+        plotting.plot_stat_map(cur_img, display_mode='z', cut_coords=coords[2:3],
+                            title=f"Blob {index} of network {i}", colorbar=False)
+
+plotting.show()
--- extract_timeseries_tar.py
+++ extract_timeseries_tar.py
@@ -9,7 +9,10 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
+import nibabel as nb
+
 import nilearn
+from nilearn.connectome import ConnectivityMeasure
 import nilearn.datasets
 import nilearn.input_data
 
@@ -17,14 +20,30 @@ from bids import BIDSLayout
 from nilearn.interfaces.fmriprep import load_confounds
 
 
-CCNA_PATH = "/data/cisl/lussier/ccna/ccna_fmriprep/fmriprep_2020/fmriprep/"
-BIDS_INFO = "/data/simexp/hwang/ccna_ts/.bids_info"
+CCNA_PATH = "/scratch/dlussier/ccna_fmriprep/1641486564/fmriprep/"
+BIDS_INFO =  Path(__file__) / ".bids_info"
 ATLAS_METADATA = {
     'difumo': {'type': "dynamic",
                'resolutions': [64],
                'label_idx': 1,
                'fetcher': "nilearn.datasets.fetch_atlas_difumo(dimension={resolution},resolution_mm=3)"},
 }
+
+
+def extract_regions_network(map_file):
+    """Extract isolated regions from network map"""
+    extractor = RegionExtractor(map_file, threshold=0.5,
+                                thresholding_strategy='ratio_n_voxels',
+                                extractor='local_regions',
+                                standardize=True, detrend=True)
+    extractor.fit()
+
+    labels = []
+    for i in range(atlas_resolution):
+        regions_indices_network = np.where(np.array(extractor.index_) == i)
+        for index in regions_indices_network[0]:
+            labels.append(f"region-{index + 1}_network-{i + 1}")
+    return extractor, labels
 
 
 def create_atlas_masker(atlas_name, nilearn_cache=""):
@@ -81,7 +100,7 @@ def bidsish_timeseries_file_name(file_entitiles, layout, atlas_name, resolution)
     """Create a BIDS-like file name to save extracted timeseries as tsv."""
     pattern = "sub-{subject}[_ses-{session}]_task-{task}[_acq-{acquisition}][_rec-{reconstruction}][_run-{run}][_echo-{echo}]"
     base = layout.build_path(file_entitiles, pattern, validate=False)
-    base += f"_atlas-{atlas_name}_nroi-{resolution}_timeseries.tsv"
+    base += f"_atlas-{atlas_name}_network-{resolution}_timeseries.tsv"
     return base.split('/')[-1]
 
 
@@ -89,17 +108,22 @@ if __name__ == '__main__':
     layout = BIDSLayout(CCNA_PATH, config=['bids','derivatives'])
     # layout.save(BIDS_INFO)
     subject_list = layout.get(return_type='id', target='subject')
-    output_root_dir = Path("/data/simexp/hwang/")
+    output_root_dir = Path.home() / "scratch"
 
     for atlas_name in ATLAS_METADATA.keys():
         print("-- {} --".format(atlas_name))
         dataset_name = f"dataset-ccna2020_atlas-{atlas_name}"
         output_dir = output_root_dir / dataset_name
         output_dir.mkdir(parents=True, exist_ok=True)
-        atlas_masker = create_atlas_masker(atlas_name)
+        atlas_resolution = 64
+        atlas = datasets.fetch_atlas_difumo(dimension=atlas_resolution,resolution_mm=3)
+        extractor, labels = extract_regions_network(atlas.maps)
+        nb.save(extractor.regions_img_,
+                output_dir / "atlas-difumo_network-{atlas_resolution}_regions.nii.gz")
+
         for subject in subject_list:
             print(f"sub-{subject}")
-            # Note for AB
+            # Note from AB
             # if multiple run, use run 2
             # if multiple session, use ses 1
             fmri = layout.get(return_type='type',
@@ -124,19 +148,25 @@ if __name__ == '__main__':
             file_entitiles = fmri[0].entities
             timeseries_root_dir = create_timeseries_root_dir(
                 file_entitiles)
-            for resolution in atlas_masker['resolutions']:
-                masker = atlas_masker[resolution]['masker']
-                output_filename = bidsish_timeseries_file_name(
-                    file_entitiles, layout, atlas_name, resolution)
-                confounds, sample_mask = load_confounds(fmri[0].path,
-                                                        strategy=['motion', 'high_pass', 'wm_csf', 'scrub', 'global_signal'],
-                                                        motion='basic', wm_csf='basic', global_signal='basic',
-                                                        scrub=5, fd_threshold=0.5, std_dvars_threshold=None,
-                                                        demean=True)
-                timeseries = masker.fit_transform(fmri[0].path, confounds=confounds, sample_mask=sample_mask)
-                labels = atlas_masker[resolution]['labels']
-                timeseries = pd.DataFrame(timeseries, columns=labels)
-                timeseries.to_csv(timeseries_root_dir / output_filename, sep='\t', index=False)
+            output_filename = bidsish_timeseries_file_name(
+                file_entitiles, layout, atlas_name, atlas_resolution)
+            confounds, sample_mask = load_confounds(fmri[0].path,
+                                                    strategy=['motion', 'high_pass', 'wm_csf', 'scrub', 'global_signal'],
+                                                    motion='basic', wm_csf='basic', global_signal='basic',
+                                                    scrub=5, fd_threshold=0.5, std_dvars_threshold=None,
+                                                    demean=True)
+            timeseries = extractor.fit_transform(fmri[0].path, confounds=confounds, sample_mask=sample_mask)
+
+            # Estimating connectomes and save for pytorch to load
+            corr_measure = ConnectivityMeasure(kind="correlation")
+            connectome = corr_measure.fit_transform([timeseries])[0]
+
+            timeseries = pd.DataFrame(timeseries, columns=labels)
+            timeseries.to_csv(timeseries_root_dir / output_filename, sep='\t', index=False)
+            connectome = pd.DataFrame(connectome, columns=labels, index=labels)
+            connectome.to_csv(timeseries_root_dir / output_filename.replace("timeseries", "connectome"), sep='\t')
+
+
     # tar the dataset
     with tarfile.open(output_root_dir / f"{dataset_name}.tar.gz", "w:gz") as tar:
         tar.add(output_dir, arcname=output_dir.name)
